d86b2a900b68f035d63e44f0f4ba3a5072536bc6
Mayaz Rakib <mohammadmrakib@gmail.com>
2024-04-15 16:09:40 +1000

    Separated starter code and working code

--- agent.py
+++ agent.py
@@ -6,23 +6,20 @@
 # Author: Mohammad Mayaz Rakib
 # zID: z5361151
 
+# USAGE:
+# ./agent.py -p <PORT>
+
 import socket
 import sys
-import numpy as np
+import numpy as np # type: ignore
 
 #
 # Game state/model
 #
 
-# a board cell can hold:
-#   0 - Empty
-#   1 - We played here
-#   2 - Opponent played here
-
-# the boards are of size 10 because index 0 isn't used
 boards = np.zeros((10, 10), dtype="int8")
 s = [".","X","O"]
-curr = 0 # this is the current board to play in
+curr = 0 
 
 #
 # Static evaluation of game state
@@ -40,36 +37,28 @@ curr = 0 # this is the current board to play in
 # Actions
 #
 
-# choose a move to play
 def play():
-    # print_board(boards)
-
-    # just play a random move for now
     n = np.random.randint(1,9)
     while boards[curr][n] != 0:
         n = np.random.randint(1,9)
 
-    # print("playing", n)
     place(curr, n, 1)
     return n
 
-# place a move in the global boards
 def place( board, num, player ):
     global curr
     curr = num
     boards[board][num] = player
 
 #
-# Printing/Debug
+# Debug
 #
 
-# print a row
 def print_board_row(bd, a, b, c, i, j, k):
     print(" "+s[bd[a][i]]+" "+s[bd[a][j]]+" "+s[bd[a][k]]+" | " \
              +s[bd[b][i]]+" "+s[bd[b][j]]+" "+s[bd[b][k]]+" | " \
              +s[bd[c][i]]+" "+s[bd[c][j]]+" "+s[bd[c][k]])
 
-# Print the entire board
 def print_board(board):
     print_board_row(board, 1,2,3,1,2,3)
     print_board_row(board, 1,2,3,4,5,6)
@@ -88,8 +77,6 @@ def print_board(board):
 # Server
 #
 
-# read what the server sent us and
-# parse only the strings that are necessary
 def parse(string):
     if "(" in string:
         command, args = string.split("(")
@@ -98,36 +85,16 @@ def parse(string):
     else:
         command, args = string, []
 
-    # init tells us that a new game is about to begin.
-    # start(x) or start(o) tell us whether we will be playing first (x)
-    # or second (o); we might be able to ignore start if we internally
-    # use 'X' for *our* moves and 'O' for *opponent* moves.
-
-    # second_move(K,L) means that the (randomly generated)
-    # first move was into square L of sub-board K,
-    # and we are expected to return the second move.
     if command == "second_move":
-        # place the first move (randomly generated for opponent)
         place(int(args[0]), int(args[1]), 2)
-        return play()  # choose and return the second move
-
-    # third_move(K,L,M) means that the first and second move were
-    # in square L of sub-board K, and square M of sub-board L,
-    # and we are expected to return the third move.
+        return play()
     elif command == "third_move":
-        # place the first move (randomly generated for us)
         place(int(args[0]), int(args[1]), 1)
-        # place the second move (chosen by opponent)
         place(curr, int(args[2]), 2)
-        return play() # choose and return the third move
-
-    # nex_move(M) means that the previous move was into
-    # square M of the designated sub-board,
-    # and we are expected to return the next move.
+        return play()
     elif command == "next_move":
-        # place the previous move (chosen by opponent)
         place(curr, int(args[0]), 2)
-        return play() # choose and return our next move
+        return play()
 
     elif command == "win":
         print("Yay!! We win!! :)")
@@ -139,10 +106,9 @@ def parse(string):
 
     return 0
 
-# connect to socket
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port = int(sys.argv[2]) # Usage: ./agent.py -p (port)
+    port = int(sys.argv[2])
 
     s.connect(('localhost', port))
     while True:
@@ -157,5 +123,5 @@ def main():
             elif response > 0:
                 s.sendall((str(response) + "\n").encode())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
